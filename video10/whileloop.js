5949f39d2205038772b4a399792fa8d9143f1969
ataldev887 <ataldev887@gmail.com>
2024-06-29 10:57:12 +0530

    while do while loop

---
+++ video10/whileloop.js
@@ -0,0 +1,19 @@
+// while loop
+// first condition check and then work done
+let n = prompt("Enter the value of n");
+n = Number.parseInt(n);
+let i =0;
+while(i<n){
+    console.log(i);
+    i++;
+}
+
+// do while
+// first work done then condition check
+let v = prompt("Enter the value of v");
+v = Number.parseInt(v);
+let k =10;
+do{
+    console.log(k);
+    i++;
+}while(k<v)
