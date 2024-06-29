24e3e26b724aab6a44ddd373ee4ea5b28c1da2bc
ataldev887 <ataldev887@gmail.com>
2024-06-29 12:44:40 +0530

    methods in strings

---
+++ video14/strings1.js
@@ -0,0 +1,14 @@
+let name = "harrybhai"
+let name1 = "HARRY"
+let friend = "Prakash"
+let friend2 = "         Vaibhav         "
+console.log(name.length) // property
+
+console.log(name.toUpperCase()) //function
+console.log(name1.toLowerCase())
+console.log(name.slice(2,4)) //print from 2 letter to 3 4th will not be included
+console.log(name.slice(2))  //print from 2 letter to end
+console.log(name.replace("bhai","bhau"))
+console.log(name.concat(" is a friend of ", friend))
+console.log(friend2)
+console.log(friend2.trim())
