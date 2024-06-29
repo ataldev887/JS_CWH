346e1b147d90478934482341178cc94af8c1c558
ataldev887 <ataldev887@gmail.com>
2024-06-29 12:09:49 +0530

    Strings intro

---
+++ video13/strings.js
@@ -0,0 +1,20 @@
+// string is collection of character
+let name = "Harry"
+console.log(name.length);
+console.log(name[0])
+console.log(name[1])
+console.log(name[2])
+
+let friend = 'Dev'
+console.log(friend)
+
+// template literals
+let boy1 = "Pramod";
+let boy2 = "Nikhil";
+let sentence = `${boy1} is friend of ${boy2}`
+console.log(sentence)
+
+// escape sequence characters
+let greet = 'Hel\'lo'
+console.log(greet)
+console.log(greet.length)
