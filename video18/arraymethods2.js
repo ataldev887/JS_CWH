58872ec6503abed55c1c459a728adcd4a761886d
ataldev887 <ataldev887@gmail.com>
2024-06-30 09:10:35 +0530

    some more array methods

--- video18/arraymethods2.js
+++ video18/arraymethods2.js
@@ -17,3 +17,12 @@ let arr = [56,99,37,555,222,29,13,27]
 arr.sort(compare) //it is sorting alphabetically considering it as string
 arr.reverse()
 console.log(arr)
+
+// splice and slice
+let num1 = [551,22,3,14,5,6,7,8,229]
+let deleted = num1.splice(2,3,1021,1022,1023)   // at index 2 , (3,14,5) will be removed and 1021,1022,1023 will be added
+console.log(deleted) // deleted elements in the form of array
+console.log(num1)
+
+const newNum = num1.slice(2)
+console.log(newNum)
