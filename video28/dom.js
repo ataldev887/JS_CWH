67287866e826b85838c30d9b39f51b7a0f7bea90
ataldev887 <ataldev887@gmail.com>
2024-07-02 11:31:52 +0530

    BOM, DOM in JS

---
+++ video28/dom.js
@@ -0,0 +1,4 @@
+console.log(window)
+document.body.style.background="green"  
+// we can style html page through document object model
+// we should use dom on style when user clicks something related to it
