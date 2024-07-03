4150b605d8a99e649591f68d12b131079d6dc5de
ataldev887 <ataldev887@gmail.com>
2024-07-03 08:31:42 +0530

    walk dom in js

---
+++ video31/walkdom.js
@@ -0,0 +1,9 @@
+document.documentElement //it gives whole html of the page returns object
+
+document.title //it gives u title of the page and returns string
+
+typeof document.title
+
+document.body //it gives page body tag returns object
+
+document.head //it gives head tag of the page returns object
