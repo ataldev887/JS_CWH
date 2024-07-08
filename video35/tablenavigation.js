be8a70156bc503a09543cb9384070725e8fc0f92
ataldev887 <ataldev887@gmail.com>
2024-07-08 18:02:32 +0530

    Table navigation

---
+++ video35/tablenavigation.js
@@ -0,0 +1,8 @@
+let t = document.body.firstElementChild.firstElementChild
+console.log(t)
+console.log(t.rows)
+console.log(t.tHead)
+console.log(t.tHead.firstElementChild)
+console.log(t.tFoot)
+console.log(t.tBodies)
+console.log(t.caption)
